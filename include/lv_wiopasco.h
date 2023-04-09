fca526c1bb6731d59a23712330c9a5f59d35e4a8
Jason Russell <grayhatguy@grayhatguy.com>
2023-04-09 00:21:51 -0400

    Add files via upload

---
+++ include/lv_wiopasco.h
@@ -0,0 +1,39 @@
+/**
+ * @file lv_wiopasco.h
+ *
+ */
+
+#ifndef LV_WIOPASCO_H
+#define LV_WIOPASCO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lvgl.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_wiopasco(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_WIOPASCO_H*/
